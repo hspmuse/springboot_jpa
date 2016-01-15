33738e2fe69e7652d18c1907c9e3f38f26f5b6f1
1002428 <1002428@SKP1002428MN001.local>
2016-01-15 19:01:36 +0900

    test

---
+++ spring_jpa1/src/main/java/com/example/SpringbootJpa1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpa1Application.class, args);
+	}
+}
